0da510a01bd4624190488035724981b3201d876b
Nuzla Ismail <8539123+nuzla@users.noreply.github.com>
2023-04-20 01:21:52 +1200

    Add the pggb_test.sh

---
+++ Scripts/pggb_test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+module load Singularity
+#export container to a variable for convenience
+WD=/nesi/nobackup/ga03793/pg_workshop #Working Directory
+container=/nesi/project/ga03793/software/pggb/pggb_0.5.3.simg
+data=${WD}/4Sim.fa
+
+#Bind filesystem to container image 
+export SINGULARITY_BIND="${WD}, /nesi/project/ga03793/"
+
+singularity exec ${container} pggb -i $data -s 1000 -p 95 -n 8 -k 79 -t 2 -S -m -o output 
