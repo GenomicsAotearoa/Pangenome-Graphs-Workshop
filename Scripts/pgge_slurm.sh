4d33ecd849d15f5e585b90b51453966e11ffbbcb
Nuzla Ismail <8539123+nuzla@users.noreply.github.com>
2023-04-27 05:01:51 +1200

    Uploadd pgge_slurm.sh script

---
+++ Scripts/pgge_slurm.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+#SBATCH --account       ga03793
+#SBATCH --job-name      4Sim_pgge
+#SBATCH --cpus-per-task 8 
+#SBATCH --mem           4G
+#SBATCH --time          1:00:00
+
+module purge
+module load Singularity
+#export container to a variable for convenience
+WD=/nesi/nobackup/ga03793/pg_workshop #Working Directory
+container=/nesi/project/ga03793/software/pgge/pgge_032023.simg
+data=${WD}/4Sim.fa
+
+#Bind filesystem to container image 
+export SINGULARITY_BIND="${WD}, /nesi/project/ga03793/"
+
+singularity exec ${container} pgge -g ${WD}/output/*.gfa -f $data -o pgge_output -r ${WD}/beehave.R -b pgge_output/pgge_4Sim_peanut_bed -l 100000 -s 5000 -t 8 
