c08e9449be066b840faac7aa09ffc22a5f159ff1
Nuzla Ismail <8539123+nuzla@users.noreply.github.com>
2023-05-21 05:34:45 +1200

    Add files via upload

--- Scripts/pggb_test.sh
+++ Scripts/pggb_test.sh
@@ -3,9 +3,9 @@ module load Singularity
 #export container to a variable for convenience
 WD=/nesi/nobackup/ga03793/pg_workshop #Working Directory
 container=/nesi/project/ga03793/software/pggb/pggb_0.5.3.simg
-data=${WD}/4Sim.fa
+data=${WD}/ASM19152v1_pgsim.fa
 
 #Bind filesystem to container image 
 export SINGULARITY_BIND="${WD}, /nesi/project/ga03793/"
 
-singularity exec ${container} pggb -i $data -s 1000 -p 95 -n 4 -k 79 -t 2 -S -m -o output -V 'NC_neisseria:#' 
+singularity exec ${container} pggb -i $data -s 1000 -p 95 -n 6 -k 79 -t 2 -S -m -o output -V 'NC_017518.1:#' 
